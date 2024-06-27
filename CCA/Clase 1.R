5c6daa1267aa6b7103253a02cabe403f35385b68
treginelli <reginellitomas@gmail.com>
2024-06-27 17:47:45 -0300

    Update Clase 1.R

--- CCA/Clase 1.R
+++ CCA/Clase 1.R
@@ -1,15 +1,15 @@
 ##########################################################
-#         COMPUTACIÓN CIENTIFICA ACTUARIAL (746)         #
-#           CATEDRA DEPARTAMENTO DE MATEMATICA           #
-#	                CARRERA DE ACTUARIO                    #
-#            FACULTAD DE CIENCIAS ECONÓMICAS             #
-#              UNIVERSIDAD DE BUENOS AIRES               #
+#    ESTADISTICA ACTUARIAL (751) - CATEDRA DE HONOR -    #
+#      Prof. Consulto Alberto Hector Landro              #
+#	           CARRERA DE ACTUARIO                         #
+#         FACULTAD DE CIENCIAS ECON?MICAS                #
+#           UNIVERSIDAD DE BUENOS AIRES                  #
 ##########################################################
 
-## Asignatura: COMPUTACIÓN CIENTIFICA ACTUARIAL 
-## 1er Cuatrimestre de 2019
+## Asignatura: ESTAD?STICA ACTUARIAL 
+## 1er Cuatrimestre de 2021
 ## Docente: Rodrigo Del Rosso
-## Basado en una guía desarrollada por las Profs. GIRIMONTE Y MOLINARI
+## Basado en una guia desarrollada por las Profs. GIRIMONTE Y MOLINARI
 
 ################################
 ####### SETEO DE CARPETA #######
@@ -19,19 +19,21 @@ getwd()
 
 dir()
 
-directorio <- choose.dir()
+directorio<-choose.dir()
 
 setwd(directorio)
 
-setwd("C:/Users/Rodri/Desktop/Estadística Actuarial/R/Clase 1")  ## acá coloquen la ruta donde poseen los archivos de trabajo
+path = "C:/Users/santi/Desktop/Santi/EA/Clases R/Clase 1"
 
-getwd() ## verifico si me modificó la ruta
+setwd(path)
+
+getwd() ## verifico si me modifico la ruta
 
 ## permite cambiar el directorio donde estamos trabajando (working directory)
-## Hay que tener en cuenta que los datos se guardarán en ese directorio
+## Hay que tener en cuenta que los datos se guardaran en ese directorio
 
 ##############################################
-####### OPERACIONES CON NÚMEROS REALES #######
+####### OPERACIONES CON NUMEROS REALES #######
 ##############################################
    
 2+5
@@ -78,29 +80,29 @@ round(1/3,4)
 
 -100:0
 
-# La función "seq" produce secuencias equi-espaciadas, con paso que se puede determinar. Por default usa paso 1.
+# La funcion "seq" produce secuencias equi-espaciadas, con paso que se puede determinar. Por default usa paso 1.
 
 seq(1,5)
 
 seq(1,5,0.5)
 
-# Con la función rep replica un valor dado la cantidad de veces que se le indique: rep(valor, longitud)
+# Con la funcion rep replica un valor dado la cantidad de veces que se le indique: rep(valor, longitud)
 
 rep(2,5)
 
 ##########################
-####### ASIGNACIÓN #######
+####### ASIGNACION #######
 ##########################
 
-## Operador de asignación “<-“: nos permite asignar un nombre/valor/formato a un objeto
+## Operador de asignacion "<-": nos permite asignar un nombre/valor/formato a un objeto
  
-x <- seq(1,5)
+x<-seq(1,5)
 
 x
 
 print(x)
 
-## Se distinguen mayúsculas de minúsculas. Por ejemplo, si ahora le pedimos que nos muestre X
+## Se distinguen mayusculas de minusculas. Por ejemplo, si ahora le pedimos que nos muestre X
 
 X
 
@@ -110,40 +112,41 @@ X
 ####### VECTORES #######
 ########################
 
-x <- c(1,3,5,7)
+
+x<-c(1,3,5,7)
 x
 
-x <- seq(1,5)
+x<-seq(1,5)
 x
 
-y <- seq(1,10,2)
+y<-seq(1,10,2)
 y
  
 x*y    # Multiplica componente a componente
 
 x%*%y   # Realiza el producto escalar entre vectores.
 
-w <- c(1,2,3)
+w<-c(1,2,3)
   
-x + w
+x+w
 
-# Ejemplo de vector lógico
+# Ejemplo de vector l?gico
  
-x <- c(T,F,F,T) # un vector lógico contiene True o False en cada componente.
+x<-c(T,F,F,T) # un vector l?gico contiene True o False en cada componente.
 x
 
 # Ejemplo de caracteres 
 
-x <- "Hoy es lunes" # vector de caracteres de longitud 1
-y <- c("Juan","Tomas","Sol") # vector de caracteres de longitud 3
+x<-"Hoy es lunes" # vector de caracteres de longitud 1
+y<-c("Juan","Tomas","Sol") # vector de caracteres de longitud 3
 
 #3.3 Algunas funciones sobre vectores
   
-x <- seq(1,5)
+x<-seq(1,5)
 
 length(x)  # devuelve la dimension del vector
 
-dim(x) ## ¿Porqué da Null?
+dim(x) ## Â¿Porque da Null?
 
 is.vector(x) #preguntamos si el objeto que generamos es un vector
 
@@ -155,7 +158,7 @@ sort(c("Juan", "Tomas", "Sol"))
      
 order((c("Juan", "Tomas", "Sol")))  # indica la componente del vector original que queda en cada lugar al ordenar.
 
-x <- c("Juan", "Tomas", "Sol")
+x<-c("Juan", "Tomas", "Sol")
 
 
 x[2]
@@ -163,18 +166,18 @@ x[2]
 x[order(x)] # genera el mismo resultado que  sort(x)
 
 
-x <- c(1,4,TRUE,"hola")
+x<-c(1,4,TRUE,"hola")
 x
 
 data.class(x)
 
 
-y <- c(1,4,TRUE)          
+y<-c(1,4,TRUE)          
 y
 
 data.class(y)
 
-w <- c(1,4,FALSE)       
+w<-c(1,4,FALSE)       
 w
       
 ########################
@@ -183,53 +186,53 @@ w
 
 # A partir de un vector dado generar una Matriz # 
 
-x <- 1:12
+x<-1:12
 
-A <- matrix(x,nrow=3,ncol=4)
+A<-matrix(x,nrow=3,ncol=4)
 A
    
-A <- matrix(x,nrow=3)
+A<-matrix(x,nrow=3)
 A
 
-A <- matrix(x,ncol=4)
+A<-matrix(x,ncol=4)
 A
 
-B <- matrix(x,nrow=3,byrow=T)
+B<-matrix(x,nrow=3,byrow=T)
 B
 
-# Con la función scan #
+# Con la funcion scan #
 
-M <- matrix(scan(),ncol=3)
+M<-matrix(scan(),ncol=3)
 M
      
 # Podemos crear el vector en la misma sentencia #
 
-M <- matrix(c(2,3,1,2,4,5,2,4,3),ncol=3)
+M<-matrix(c(2,3,1,2,4,5,2,4,3),ncol=3)
 M
 
 # Con las funciones cbind rbind #
 
-col1 <- c(1,2,3)
-col2 <- c(0,1,1)
-A <- cbind(col1,col2)
+col1<-c(1,2,3)
+col2<-c(0,1,1)
+A<-cbind(col1,col2)
 A
   
-B <- rbind(col1,col2)
+B<-rbind(col1,col2)
 B
      
 # Operaciones algebraicas.
 
-A <- matrix(1:10,ncol=5)          
+A<-matrix(1:10,ncol=5)          
 A
             
-A + c(0,1)
+A+c(0,1)
 	     
 #Si se quiere multiplicar en forma matricial se debe usar %*%:
 
-A <- matrix(1:12,ncol=4)
+A<-matrix(1:12,ncol=4)
 A
      
-B <- matrix(seq(1,24,3),ncol=2)
+B<-matrix(seq(1,24,3),ncol=2)
 B
   
 A%*%B
@@ -251,11 +254,11 @@ A[1,]   # devuelve la primera fila de la matriz
 
 A[,4]   # devuelve la cuarta columna de la matriz.
 
-A[7]    # devuelve el septimo elemento de la matriz recorriéndola por columnas.
+A[7]    # devuelve el septimo elemento de la matriz recorriendola por columnas.
 
 # Operaciones por filas o columnas #
 
-A <- matrix(1:20,ncol=5)
+A<-matrix(1:20,ncol=5)
 A
     
 apply(A,2,sum)  #Sumamos las columnas de la matriz A
@@ -268,14 +271,14 @@ apply(A,1,sd)
 
 # Nombres de las filas y columnas de una matriz #
 
-notas <- c(10,9,10,9,8,10)
-notas <- matrix(notas,3,2,byrow = T)
+notas<-c(10,9,10,9,8,10)
+notas<-matrix(notas,3,2,byrow=T)
 notas
      
-rownames(notas) <- c("Juan","Ana","Pedro")
+rownames(notas)<-c("Juan", "Ana","Pedro")
 notas
      
-colnames(notas) <- c("Parcial","Final")
+colnames(notas)<-c("Parcial", "Final")
 notas
       
 dimnames(notas)
@@ -286,55 +289,54 @@ notas["Juan",]
 notas[,"Final"]
  
 
-x <- 1:12  
+x<- 1:12  
 x
 dim(x)
 
-x <- as.matrix(x)  # transformamos a x en una matriz
-
-dim(x)             # dim nos devuelve las dimensiones de la matriz
+x<-as.matrix(x)  #transformamos a x en una matriz
+dim(x)           # dim nos devuelve las dimensiones de la matriz
 
 is.matrix(x)       # preguntamos si es una matriz
 
-data.class(x)      # preguntamos qué tipo de objeto es
+data.class(x)       # preguntamos que tipo de objeto es
 
 ############################
 ####### DATAS FRAMES #######
 ############################
 
-# Ejemplos de construcción de un data frame #
+# Ejemplos de construccion de un data frame #
 
-# Ejercicio 1 # 
+#Ej 1 
 
-x1 <- c("Juan","Sol","Tomás")
-x2 <- c(10,9,8)
-x3 <- c(9,9,7)
-x4 <- c(TRUE,FALSE,TRUE)
-Notas <- data.frame(x1,x2,x3,x4)
+x1<-c("Juan","Sol","Tom?s")
+x2<-c(10,9,8)
+x3<-c(9,9,7)
+x4<-c(TRUE,FALSE,TRUE)
+Notas<-data.frame(x1,x2,x3,x4)
 Notas
 
-colnames(Notas) <- c("Nombre","Primer parcial","Segundo Parcial","Tutoría")
+colnames(Notas)<-c("Nombre","Primer_parcial","Segundo_Parcial","Tutor?a")
 Notas
 
-# Ejercicio 2 # 
+#Ej 2
 
-x <- c(1,1,1,1)
-y <- c(2,2,2)
-z <- data.frame(x,y)
+x<-c(1,1,1,1)
+y<-c(2,2,2)
+z<-data.frame(x,y)
 
-# Ejercicio 3 # 
+#Ej 3
 
-y <- c(2,2,2,NA)
-z <- data.frame(x,y)
+y<-c(2,2,2,NA)
+z<-data.frame(x,y)
 z
   
-# Acceso a los elementos de un Data Frame #
+# Acceso a los elentos de un data frame #
 
 Notas[,1] 
 
 Notas$Nombre
 
-Notas$Primer Parcial    ## Qué error posee esta línea de código ##
+Notas$Primer_Parcial
 
 Notas$Primer 
 
@@ -345,31 +347,32 @@ apply(Notas[,2:4],2,mean)
  
 apply(Notas,2,mean)
        
-# Podemos transformar una matriz en un Data Frame #
+# Podemos transformar una matriz en un data frame #
 
-A <- matrix(1:10,2,5)
+A<-matrix(1:10,2,5)
  
 data.class(A)
 
-Adf <- data.frame(A)
+Adf<-data.frame(A)
  
 data.class(Adf)
 
-B <- matrix(1:15,ncol = 3)
+B<-matrix(1:15,ncol=3)
 data.class(B)
-
-Bdf <- as.data.frame(B)
+Bdf<-as.data.frame(B)
 data.class(Bdf)
 
-#5.3 Cálculos sobre subgrupos de datos
 
-sexo <- c("M","F","M")
+#5.3 Calculos sobre subgrupos de datos
 
-Notas2 <- data.frame(x1,x2,x3,sexo)
+sexo<-c("M","F","M")
 
-colnames(Notas2) <- c("Nombre","Primer parcial","Segundo Parcial","Género")
+Notas2<-data.frame(x1,x2,x3,sexo)
 
-# Calculemos el promedio en cada uno de los parciales por género
+colnames(Notas2)<-c("Nombre","Primer parcial","Segundo Parcial","G?nero")
+
+  
+# Calculemos el promedio en cada uno de los parciales por genero
 
 tapply(Notas2[,2],Notas2[,4],mean)
 
@@ -382,10 +385,10 @@ tapply(Notas2[,2:3],Notas2[,4],mean)
 
 # Generando una lista #
 
-x <- matrix(1:4, 4, 7)
-y <- "Hoy es jueves"
-z <- c(T, F, T, NA)
-lista <- list(x,y,z)
+x<-matrix(1:4, 4, 7)
+y<-"Hoy es jueves"
+z<-c(T, F, T, NA)
+lista<-list(x, y, z)
 
 lista
 
@@ -393,16 +396,16 @@ lista[[2]]
 
 # Se asigna un  nombre con names(): 
 
-names(lista) <- c("Matriz", "Leyenda","Vector Logico")
+names(lista)<- c("Matriz", "Leyenda","Vector Logico")
 lista
 
-# Para referirse a un elemento de la lista, además del uso de los corchetes,  tenemos dos formas más si hay nombres asignados:
+# Para referirse a un elemento de la lista, adem?s del uso de los corchetes,  tenemos dos formas mas si hay nombres asignados:
 
 lista$Leyenda
 
 lista[["Leyenda"]]
 
-# las listas tienen asociada longitud y no dimensión. 
+# las listas tienen asociada longitud y no dimension. 
   
 length(lista)
 
@@ -411,37 +414,79 @@ names(lista)
 
 # Podemos agregar o eliminar elementos a una lista
 
-lista2 <- lista[-1] #Eliminamos el primer elemento
+lista2<-lista[-1] #Eliminamos el primer elemento
 lista2
 
-lista[[4]] <- c(1,2,3)  # Agregamos una nueva componente a nuestra lista de 3 elementos
+lista[[4]]<-c(1,2,3)  # Agregamos una nueva componente a nuestra lista de 3 elementos
 lista
 
 # Una lista particular: dimnames
 
-#Podemos agregar nombres a las filas y columnas de una matriz (no de  un data frame) creando esa lista y asignándosela a  dimnames(A). 
+#Podemos agregar nombres a las filas y columnas de una matriz (no de  un data frame) creando esa lista y asign?ndosela a  dimnames(A). 
 
-notas <- c(10,9,10,9,8,10)
-notas <- matrix(notas,3,2,byrow=T)
+notas<-c(10,9,10,9,8,10)
+notas<-matrix(notas,3,2,byrow=T)
 notas
    
-dimnames(notas) <- list(c("Juan","Ana","Pedro"),c("Parcial","Final"))
+dimnames(notas)<-list(c("Juan","Ana","Pedro"),c("Parcial","Final"))
 
 notas
      
-#Si se quiere dar nombres unicamente a las columnas ó a las filas utilice NULL: 
+#Si se quiere dar nombres unicamente a las columnas o a las filas utilice NULL: 
 
-dimnames(notas) <- list(NULL,c("Parcial", "Final"))
+dimnames(notas)<-list(NULL,c("Parcial", "Final"))
 notas
 
-dimnames(notas) <- list(c("Juan","Ana","Pedro"),NULL)
+dimnames(notas)<-list(c("Juan","Ana","Pedro"),NULL)
 notas
       
+
+#########################
+####### FUNCIONES #######
+#########################
+
+Costo<-function(horas,pph=40){
+  
+  Costo<-horas*pph
+  return(Costo)}
+
+Costo(10)
+
+Costo(10,15)
+
+
+
+Coeficiente.Variacion<-function(mean,sd){
+  
+  Coeficiente.Variacion<-sd/mean
+  return(Coeficiente.Variacion)
+}
+
+Coeficiente.Variacion(10,2)
+
+## Incorporando un condicional "if"
+
+Costo2<-function(horas,pph=40){
+  
+if(horas>100){
+    
+Costo<-horas*pph*0.90 
+    
+  } else Costo<-horas*pph
+  
+return(Costo)}
+
+Costo2(100)
+
+Costo2(120);Costo(120)
+
+Costo2(90)
+
 ###############################
 ####### OBJETOS CREADOS #######
 ###############################
 
-ls()   # fíjese cuáles son los objetos que ha creado.     
+ls()   # f?jese cu?les son los objetos que ha creado.     
 
 objects() ## otra forma de listar todos los objetos creados
          
